712368d5e393fc41c2877c2bfe6d5c4a0028ff30
sophie <a.sophiewirth@gmail.com>
2017-03-05 20:19:38 +0100

    Set up database for Heroku

--- thesis/settings.py
+++ thesis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,12 +77,25 @@ WSGI_APPLICATION = 'thesis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ['DATABASE_URL']:
+    db_from_env = dj_database_url.config()
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        }
     }
-}
+
+    DATABASES['default'].update(db_from_env)
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'thesis',
+            ''
+        }
+    }
 
 
 # Password validation
